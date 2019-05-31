c9867ca50f53b15d8f407836e4a38fc4edb22b6b
psc1453 <pscpeng@outlook.com>
2019-05-31 09:43:05 +0800

    add header comment

--- LCD1602.h
+++ LCD1602.h
@@ -14,20 +14,25 @@ private:
     DigitalOut RW;
     DigitalOut E;
     BusOut DATA;
-    char buffer[32];
     void pulse_E();
 
 public:
+    //pin definition
     LCD(PinName rs, PinName rw, PinName e,
         PinName d4, PinName d5, PinName d6, PinName d7);
+    //use this method first
     void initial();
+    //clear the display
     void erase();
+    //display static content
     int display(const char *output, int mode = 1);//mode1 auto next line mode0 not
+    //rolling display
     int rolling(const char *output);
-
+    //use like printf
     template<typename...T>
     int printf(const char *fmt,T...args)
     {
+        char buffer[32];
         sprintf(buffer, fmt,args...);
         return display(buffer);
     }
